a5462a3b2f2a889607e3312c5ba4c43304193b95
Vafo <v.safokhujaev@student.inha.uz>
2022-11-09 23:09:42 +0000

    blink_hal organization. removed irrelevant code
    Related to #13
    Related to #16

--- blink_hal.c
+++ blink_hal.c
@@ -1,15 +1,33 @@
 #include "blink_hal.h"
 
+#ifdef BOARD_PCA10059
+    #define LED_1_GPIO       NRF_GPIO_PIN_MAP(0,6)
+    #define LED_2R_GPIO      NRF_GPIO_PIN_MAP(0,8)
+    #define LED_2G_GPIO      NRF_GPIO_PIN_MAP(1,9)
+    #define LED_2B_GPIO      NRF_GPIO_PIN_MAP(0,12)
+    
+    #define LEDS {LED_1_GPIO, LED_2R_GPIO, LED_2G_GPIO, LED_2B_GPIO}
+#endif
+
 static uint8_t leds_gpio[LEDS_NUMBER] = LEDS;
 
-void led_init(void)
+void led_init_all(void)
 {
-    for(uint8_t i = 0; i < LEDS_NUMBER; i++)
+    static uint8_t leds_initialized = 0;
+    if(leds_initialized == 0)
     {
-        nrf_gpio_cfg_output(leds_gpio[i]);
-        led_off(i);
+        for(uint8_t i = 0; i < LEDS_NUMBER; i++)
+        {
+            led_init(i);
+        }
     }
+    leds_initialized = 1;
+}
 
+void led_init(uint32_t led)
+{
+    nrf_gpio_cfg_output(leds_gpio[led]);
+    led_off(led);
 }
 
 void led_off(uint32_t led)
--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 
 #include "blink_hal.h"
 #include "button_hal.h"
+#include "button_hold.h"
+
 
 #define DEVICE_ID 7199
 #define BLINK_DURATION 500 // ms
@@ -13,6 +15,18 @@
 
 #define SLEEP_FRACTION_MS 10
 
+// Blinks error state infinitely
+void blink_error()
+{
+    led_init(LED_2RED_IDX);
+    while(true)
+    {
+        led_on(LED_2RED_IDX);
+        nrf_delay_ms(250);
+        led_off(LED_2RED_IDX);
+        nrf_delay_ms(250);
+    }
+}
 
 void id_to_led_blink(int num, int (*led_blink)[LEDS_NUMBER])
 {
@@ -24,64 +38,7 @@ void id_to_led_blink(int num, int (*led_blink)[LEDS_NUMBER])
     }
 }
 
-/**
- * @brief Function for application main entry.
- */
-// #define BTN_PRESS
-#define BTN_HOLD
-
-#if defined(BTN_PRESS)
-
-int main(void)
-{
-    // Configure board. 
-    led_init();
-    button_init();
-
-    int timing[LEDS_NUMBER];
-    id_to_led_blink(DEVICE_ID, &timing);
-
-    // Toggle LEDs.
-    int i = 0,
-        j = 0;
-    while (true)
-    {
-        while(button_pressed())
-        {
-            led_invert(i);
-            nrf_delay_ms(BLINK_DURATION);
-            led_invert(i);
-            nrf_delay_ms(PAUSE_DURATION);
-            j++;
-            if(j >= timing[i])
-            {
-                j = 0;
-                i++;
-            }
-            if(i >= LEDS_NUMBER)
-            {
-                i = 0;
-            }
-        }
-        
-    }
-    
-}
-
-#elif defined(BTN_HOLD)
-
-#include "button_hold.h"
-
 volatile int btn_released;
-
-
-// Extracted from nrf_gpiote.h:409 :_-(
-// Since it is encapsulated originally, maybe user is not supposed to check
-// whether the event is set or not
-__STATIC_INLINE bool own_gpiote_event_is_set(nrfx_gpiote_pin_t pin)
-{
-    return (*(uint32_t *)nrfx_gpiote_in_event_addr_get(pin) == 0x1UL) ? true : false;
-}
 
 void btn_released_on()
 {
@@ -95,6 +52,8 @@ void btn_released_off()
 
 // FIXME ?
 // This function would be valid only if sleep_ms is multiple of SLEEP_FRACTION_MS
+//
+// Notes: Maybe there is better way to wait for event to happen, and then unfreeze?
 void progress_on_hold(uint32_t sleep_ms)
 {
     int num_fractions = sleep_ms / SLEEP_FRACTION_MS;
@@ -111,23 +70,17 @@ void progress_on_hold(uint32_t sleep_ms)
 int main(void)
 {
     
-    led_init();
+    led_init_all();
     nrfx_err_t error = db_event_init(BUTTON_1, btn_released_off, btn_released_on);
     if(error != NRFX_SUCCESS)
     {
-        while(true)
-        {
-            led_on(LED_2RED_IDX);
-            nrf_delay_ms(250);
-            led_off(LED_2RED_IDX);
-            nrf_delay_ms(250);
-        }
+        blink_error();
     }
 
     int timing[LEDS_NUMBER];
     id_to_led_blink(DEVICE_ID, &timing);
 
-    // Toggle LEDs.
+    // Workaround for the very first button press
     btn_released_on();
     progress_on_hold(0);
     while(true)
@@ -148,7 +101,6 @@ int main(void)
     
 }
 
-#endif
 
 /**
  *@}
