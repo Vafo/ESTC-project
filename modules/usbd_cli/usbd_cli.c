36b51248676874a3249f805cbc88f91d8c5070fd
vafo <v.safokhujaev@student.inha.uz>
2023-05-19 10:40:50 +0500

    init of usb cli
    Related to #40

---
+++ modules/usbd_cli/usbd_cli.c
@@ -0,0 +1,117 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "nordic_common.h"
+
+#include "nrf_log.h"
+
+#include "app_usbd.h"
+#include "app_usbd_serial_num.h"
+#include "app_usbd_cdc_acm.h"
+
+#include "usbd_cli.h"
+
+#define READ_SIZE 1
+
+static char m_rx_buffer[READ_SIZE];
+
+static void usb_ev_handler(app_usbd_class_inst_t const * p_inst,
+                           app_usbd_cdc_acm_user_event_t event);
+
+/* Make sure that they don't intersect with LOG_BACKEND_USB_CDC_ACM */
+#define CDC_ACM_COMM_INTERFACE  2
+#define CDC_ACM_COMM_EPIN       NRF_DRV_USBD_EPIN3
+
+#define CDC_ACM_DATA_INTERFACE  3
+#define CDC_ACM_DATA_EPIN       NRF_DRV_USBD_EPIN4
+#define CDC_ACM_DATA_EPOUT      NRF_DRV_USBD_EPOUT4
+
+APP_USBD_CDC_ACM_GLOBAL_DEF(usb_cdc_acm,
+                            usb_ev_handler,
+                            CDC_ACM_COMM_INTERFACE,
+                            CDC_ACM_DATA_INTERFACE,
+                            CDC_ACM_COMM_EPIN,
+                            CDC_ACM_DATA_EPIN,
+                            CDC_ACM_DATA_EPOUT,
+                            APP_USBD_CDC_COMM_PROTOCOL_NONE);
+
+
+static void usb_ev_handler(app_usbd_class_inst_t const * p_inst,
+                           app_usbd_cdc_acm_user_event_t event)
+{
+    switch (event)
+    {
+    case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
+    {
+        ret_code_t ret;
+        rx_buffer_idx = 0;
+        ret = app_usbd_cdc_acm_read(&usb_cdc_acm, &m_rx_buffer[rx_buffer_idx], READ_SIZE);
+        UNUSED_VARIABLE(ret);
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
+    {
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
+    {
+        NRF_LOG_INFO("tx done");
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_RX_DONE:
+    {
+        ret_code_t ret;
+        do
+        {
+            /*Get amount of data transfered*/
+            size_t size = app_usbd_cdc_acm_rx_size(&usb_cdc_acm);
+            NRF_LOG_INFO("rx size: %d", size);
+
+            /* It's the simple version of an echo. Note that writing doesn't
+             * block execution, and if we have a lot of characters to read and
+             * write, some characters can be missed.
+             */
+            if (m_rx_buffer[0] == '\r' || m_rx_buffer[0] == '\n')
+            {
+                ret = app_usbd_cdc_acm_write(&usb_cdc_acm, "\r\n", 2);
+            }
+            else
+            {
+                ret = app_usbd_cdc_acm_write(&usb_cdc_acm,
+                                             m_rx_buffer,
+                                             READ_SIZE);
+            }
+
+            /* Fetch data until internal buffer is empty */
+            ret = app_usbd_cdc_acm_read(&usb_cdc_acm,
+                                        m_rx_buffer,
+                                        READ_SIZE);
+        } while (ret == NRF_SUCCESS);
+
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+bool usbd_cli_process()
+{
+    while (app_usbd_event_queue_process())
+    {
+        /* Nothing to do */
+    }
+
+    return true;
+}
+
+
+void usbd_cli_init()
+{
+
+    NRF_LOG_INFO("Starting up the test project with USB logging");
+
+    app_usbd_class_inst_t const * class_cdc_acm = app_usbd_cdc_acm_class_inst_get(&usb_cdc_acm);
+    ret_code_t ret = app_usbd_class_append(class_cdc_acm);
+    APP_ERROR_CHECK(ret);
+}
---
+++ modules/usbd_cli/usbd_cli.h
@@ -0,0 +1,7 @@
+#ifndef USBD_CLI_H
+#define USBD_CLI_H
+
+void usbd_cli_init();
+bool usbd_cli_process();
+
+#endif
